10ad07b16cae62d9f17254885183461571403b3c
burkeds <burkeds@mcmaster.ca>
2022-12-15 09:27:40 -0500

    initial commit

---
+++ cnbl/reduction.py
@@ -0,0 +1,103 @@
+def circular_binning(img, radius, center=(0, 0)):
+    """
+    Pixels that fall within a circle of radius r about a center point at (m1, m2):
+    P = {(x,y) : (x-m1)^2 + (y-m2)^2 <= r^2}
+
+    This function returns a list of tuples corresponding to these points.
+
+    :param img: A 2D numpy array. This is usually 2D detector array data.
+    :param radius: Radius of the circular bin about a center point.
+    :param center: The index (row, col) of the circle center point.
+    :return indices: A list of tuples containing the (row, col) indices of pixels that fall within the circle.
+    """
+
+    radius = float(radius)
+    indices = []
+    radius_squared = radius * radius
+
+    for y, row in enumerate(img):
+        for x, _ in enumerate(row):
+            dx = x-center[1]
+            dy = y-center[0]
+            distance_squared = float(dx * dx + dy * dy)
+
+            if distance_squared <= radius_squared:
+                indices.append((y, x))
+    return indices
+
+
+def ring_binning(outer, inner):
+    """
+    This function takes two lists of indices corresponding to pixels that fall within a circular bin. These are usually
+    obtained from the circular_binning function. Using list comprehension, the indices from a small inner circle are
+    subtracted from the indices of a larger outer circle to produce a list of indices that corresponds to all pixels
+    that fall within the ring between the two.
+
+    :param outer: A list of (row, col) tuples corresponding to pixel indices within a circle of radius r_outer about a
+                  center point (y_center, x_center). r_outer must satisfy the condition r_outer > r_inner.
+    :param inner: A list of (row, col) tuples corresponding to pixel indices with a circle of radius r_inner about a
+                  center point (y_center, x_center). r_inner must satisfy the condition r_inner < r_outer.
+    :return ring: A list of (row, col) tuples corresponding to pixels that fall within a ring bounded by r_outer and
+                  r_inner.
+    """
+
+    ring = [x for x in outer if x not in inner]
+    return ring
+
+
+def get_intensity(img, indices):
+    """
+    This function takes a 2D array of counts and a list of (row, col) indices. It returns the average of counts found at
+    points corresponding to the list of indices and returns an integer of intensity.
+
+    :param img: A 2D array of counts, usually array data.
+    :param indices: A list of (row, col) tuples corresponding to valid indices with img.
+    :return intensity: The sum of all elements within img whose indices (row, col) can be found within the param
+                       'indices'.
+    """
+    intensity = 0
+    for y, row in enumerate(img):
+        for x, val in enumerate(row):
+            point = (y, x)
+            if point in indices:
+                intensity += int(val)
+    intensity = intensity / len(indices)
+    return intensity
+
+
+def get_intensity_as_a_function_of_radius_in_pixels(img, center=(0, 0), n_bins=100):
+    """
+    Returns a list of radially averaged scattered intensities and their associated radial bins in pixels.
+
+    :param img: A 2D array of image data.
+    :param center: A tuple of the (row, col) index of the pixel at the center of the beam center or other point of
+                   interest.
+    :param n_bins: The number of bins used by radial binning methods.
+    :return intensities: A list of radially averaged intensities.
+    :return bins: A list defining the edges (in units of pixels) of radial bins used for averaging.
+    """
+    # Get the distance to the pixel farthest from the center point
+    from math import sqrt
+    distance_to_farthest_pixel = 0.0
+    for y, row in enumerate(img):
+        for x, _ in enumerate(row):
+            dx = x - center[1]
+            dy = y - center[0]
+            distance = sqrt(dx * dx + dy * dy)
+            if distance > distance_to_farthest_pixel:
+                distance_to_farthest_pixel = distance
+    # Make list of tuples where each element contains in inner and outer diameter of a ring
+    from numpy import linspace
+    bins = linspace(0.0, distance_to_farthest_pixel, num=n_bins)
+
+    # Use bins as ring boundaries to calculate a list of intensities
+    intensities = []
+    for i, _ in enumerate(bins):
+        if i == (len(bins) - 1):
+            continue
+        inner = circular_binning(img, bins[i], center)
+        outer = circular_binning(img, bins[i+1], center)
+        ring = ring_binning(outer, inner)
+        intensity = get_intensity(img, ring)
+        intensities.append(intensity)
+    return intensities, bins
---
+++ main.py
@@ -0,0 +1,32 @@
+from epics import caget
+import numpy as np
+from cnbl.reduction import *
+import matplotlib.pyplot as plt
+import math
+from cnbl.utils import print_impact_matrix
+
+if __name__ == '__main__':
+    wavelength = 3.1
+    wavelength_spread = 0.17
+    detector_offset = 0.0
+    source_aperture_diameter = 5.0
+    sample_aperture_diameter = 1.27
+    source_aperture_to_sample = 537
+    sample_to_detector = 800
+
+    data1d = caget('sans:mirr2d[sans_det]-Getim2DDAQ_RBV.AVAL')
+    data2d = np.reshape(data1d, (147,147), order='F')
+    center = (111, 75)
+    n_bins = 25
+    intensities, bins = get_intensity_as_a_function_of_radius_in_pixels(data2d, center, n_bins)
+    radii = [0.7*b for b in bins]
+    angles = [math.atan(r/sample_to_detector) for r in radii]
+    qs = [4 * np.pi / 3.1 * math.sin(theta / 2) for theta in angles]
+
+    fig, ax = plt.subplots()
+    ax.bar(qs[:-1], intensities, align='edge', width=qs[1]-qs[0])
+    ax.set_xlabel("Q (/ang)")
+    ax.set_ylabel("Scattered Intensity")
+    ax.set_xscale('log')
+
+    plt.show()
