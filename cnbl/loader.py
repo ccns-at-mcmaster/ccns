f41e5c226e23da12a984bfc0fcc39bbef9cad45a
burkeds <burkeds@mcmaster.ca>
2023-01-11 13:34:28 -0500

    loader can now be used to load a nexus file and return a python dictionary with experimental data and metadata. working contains an example of how to use these methods and pass values from the dict to reduction methods.

--- cnbl/loader.py
+++ cnbl/loader.py
@@ -11,33 +11,36 @@ import h5py
 from os import path
 
 nxsas_dict = {
-    'root': '/',
-    'entry': '/entry',
     'title': '/entry/title',
     'start_time': '/entry/start_time',
     'end_time': '/entry/end_time',
-    'instrument': '/entry/instrument',
     'instrument_name': '/entry/instrument/name',
-    'source': '/entry/instrument/source',
-    'probe': '/entry/instrument/source/probe',
-    'wavelength': '/entry/instrument/monochromator/wavelength',
-    'wavelength_spread': '/entry/instrument/monochromator/wavelength_spread',
+    'source_type': '/entry/instrument/source/source_type',
+    'source_name': '/entry/instrument/source/source_name',
+    'source_probe': '/entry/instrument/source/source_probe',
+    'monochromator_wavelength': '/entry/instrument/monochromator/wavelength',
+    'monochromator_wavelength_spread': '/entry/instrument/monochromator/wavelength_spread',
     'collimation_length': '/entry/instrument/collimator/geometry/shape/size',
     'sdd': '/entry/instrument/detector/distance',
     'x_pixel_size': '/entry/instrument/detector/x_pixel_size',
     'y_pixel_size': '/entry/instrument/detector/y_pixel_size',
     'beam_center_x': '/entry/instrument/detector/beam_center_x',
     'beam_center_y': '/entry/instrument/detector/beam_center_y',
-    'sample_name': '/entry/sample',
+    'x_resolution': '/entry/instrument/detector/x_resolution',
+    'y_resolution': '/entry/instrument/detector/y_resolution',
+    'sample_name': '/entry/sample/sample_name',
     'monitor_mode': '/entry/monitor/mode',
     'monitor_preset': '/entry/monitor/preset',
     'monitor_integral': '/entry/monitor/integral',
     'data': '/entry/data/data',
-    'metadata': '/entry/metadata'
+    'metadata_detector_height': '/entry/instrument/metadata/detector_height',
+    'metadata_counting_time': '/entry/instrument/metadata/counting_time',
+    'metadata_sample_transmission': '/entry/instrument/metadata/sample_transmission',
+    'metadata_sample_thickness': '/entry/instrument/metadata/sample_thickness'
 }
 
 
-def get_nexus_file(filepath, mode):
+def get_nexus_file(filepath, mode='r'):
     """
     This method takes a filepath and open mode for a nexus file and returns the file.
 
@@ -60,7 +63,18 @@ def get_nexus_file(filepath, mode):
     return nexus
 
 
-if __name__ == "__main__":
-    raw_filepath = "C:\\Users\\dburk\\Desktop\\working\\user\\newrocktest0001.raw"
-    file = get_nexus_file(raw_filepath, 'r+')
-    print(file['/entry/monitor/integral'][()])
+def read_sans_raw(nexus):
+    """
+    This method takes a nexus file, extracts data and metadata from the file using the paths specified in nxsas_dict
+    and returns a dictionary of the experimental data.
+
+    :param nexus: A MacSANS raw nexus file. This should contain HDF5 databases at each path specified in nxsas_dict.
+    :return data_dict: A dictionary with key/value pairs of experimental data and metadata.
+    """
+    data_dict = {}
+    for key in nxsas_dict.keys():
+        try:
+            data_dict[key] = nexus[nxsas_dict[key]][()]
+        except KeyError:
+            data_dict[key] = None
+    return data_dict
--- working.py
+++ working.py
@@ -1,27 +1,30 @@
 from epics import caget
 import numpy as np
 from cnbl.reduction import *
+from cnbl.loader import *
 import matplotlib.pyplot as plt
 import math
 from cnbl.utils import print_impact_matrix
 
 if __name__ == '__main__':
-    wavelength = 3.1
-    wavelength_spread = 0.17
-    detector_offset = 0.0
-    source_aperture_diameter = 5.0
-    sample_aperture_diameter = 1.27
-    source_aperture_to_sample = 537
-    sample_to_detector = 800
-    sample_transmission = 0.9
-    sample_thickness = 0.5
+    filepath = "C:\\Users\\burkeds\\Desktop\\working\\rocktest0000.raw"
+    file = get_nexus_file(filepath)
+    data = read_sans_raw(file)
+    data2d = data['data']
+    center = (int(data['beam_center_y'][0]), int(data['beam_center_x'][0]))
+    sample_to_detector = data['sdd'][0] * 100
 
-    data1d = caget('sans:mirr2d[sans_det]-Getim2DDAQ_RBV.AVAL')
-    data2d = np.reshape(data1d, (147,147), order='F')
-    center = (75, 111)
+    if not data['metadata_sample_transmission']:
+        data['metadata_sample_transmission'] = 0.90
+
+    if not data['metadata_sample_thickness']:
+        data['metadata_sample_thickness'] = 0.5
+
+    sample_transmission = data['metadata_sample_transmission']
+    sample_thickness = data['metadata_sample_thickness']
     n_bins = 100
 
-    #Assume perfect empty beam
+    # Assume perfect empty beam
     empty = np.ones_like(data2d, dtype='float32')
     solid_angle_correction(data2d, center, sample_to_detector)
     pixel_solid_angle = get_pixel_solid_angle(sample_to_detector)
@@ -37,6 +40,6 @@ if __name__ == '__main__':
     ax.set_xlabel("Q (1/angstrom)")
     ax.set_ylabel("Scattered Intensity (1/cm)")
     ax.set_xscale('log')
-    #ax.set_yscale('log')
+    # ax.set_yscale('log')
 
     plt.show()
