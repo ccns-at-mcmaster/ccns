wip. Have methods to calculate Q, Qvar, scattered intensity and its standard deviation, and the beamstop factor but they break at low r_0. I should fix this and add a method to calculate <Q>.

working.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math
 from cnbl.utils import print_impact_matrix
 import numpy as np
+from cnbl.reduction.gaussiandq import get_q_statistics
 
 if __name__ == '__main__':
     # Load nexus file and read it
@@ -13,42 +14,28 @@ if __name__ == '__main__':
     data = read_sans_raw(file)
 
     # Extract useful information from the data dictionary
-    data2d = data['data']
-    data['sdd'][0] *= 100
-    sample_to_detector = data['sdd'][0]
+    data2d = data['data'].copy()
+    # Get useful metadata for reduction
+    wl = 5.0
+    wl_spread = 0.14
+    sigma_d = 0.9
+    b_s = beamstop_radius = 2.5
+    s_1 = slit_one = 5.08
+    s_2 = slit_two = 1.91
+    l_1 = source_to_sample = 1600
+    l_2 = sample_to_detector = 508
     pixel_size = data['x_pixel_size'][0]
-    wavelength = data['monochromator_wavelength'][0]
+    center = (int(data['beam_center_y'][0]), int(data['beam_center_x'][0]))
+    sample_transmission = 1.0
+    sample_thickness = 0.2
+    illuminated_sample_area = 3.14
 
-    # Set some data arbitrarily
-    data['metadata_counting_time'] = np.array([600.0])
-    data['monitor_integral'] = np.array([8.42128E+06])
+    # This is done at the EPICS level, per pixel. I will keep it here now, but it may have to change. Now, efficiency is
+    # accounting for at data acquisition and during reduction.
+    detector_efficiency = 0.7
 
-    # These values may not be in the data dict, they will be implemented in the future
-    if 'metadata_sample_transmission' in data:
-        if not data['metadata_sample_transmission']:
-            data['metadata_sample_transmission'] = np.array([1.0])
-    else:
-        data['metadata_sample_transmission'] = np.array([1.0])
-
-    if 'metadata_sample_thickness' in data:
-        if not data['metadata_sample_thickness']:
-            data['metadata_sample_thickness'] = np.array([0.2])
-    else:
-        data['metadata_sample_thickness'] = np.array([0.2])
-
-    if 'metadata_sample_area' in data:
-        if not data['metadata_sample_area']:
-            data['metadata_sample_area'] = np.array([3.14])
-    else:
-        data['metadata_sample_area'] = np.array([3.14])
-
-    if 'detector_efficiency' in data:
-        if not data['detector_efficiency']:
-            data['detector_efficiency'] = np.array([0.7])
-    else:
-        data['detector_efficiency'] = np.array([0.7])
-
-    n_bins = 100
+    counting_time = 600.0
+    monitor_counts = 8.42128E+06
 
     # Visualize the raw data
     print_impact_matrix(data2d)
@@ -69,34 +56,53 @@ if __name__ == '__main__':
     empty = np.ones_like(data2d, dtype='float32')
 
     # Perform solid angle correction on data
-    solid_angle_correction(data2d, data)
+    solid_angle_correction(data2d, l_2, center, pixel_size)
 
     # Scale data to absolute intensity
-    scale_to_absolute_intensity(data2d, empty, data)
+    scale_to_absolute_intensity(data2d,
+                                empty,
+                                sample_transmission,
+                                sample_thickness,
+                                sample_to_detector,
+                                illuminated_sample_area,
+                                detector_efficiency,
+                                counting_time,
+                                monitor_counts,
+                                normalize_time=False)
 
     # Create estimate of incoherent scattering from sample transmission and subtract from the measured values
-    incoherent = estimate_incoherent_scattering(data)
+    incoherent = estimate_incoherent_scattering(l_2, sample_transmission, data2d.shape)
     data2d = np.subtract(data2d, incoherent)
 
-    # Get a histogram of radially averaged intensities
-    # Wide angle correction is performed during radial averaging.
-    intensities, bins = get_intensity_as_a_function_of_radius_in_pixels(data2d, data, n_bins)
+    # Visualize the processed data
+    print_impact_matrix(data2d, title="Processed Data")
 
-    # Convert bins in units of pixels to physical lengths
-    radii = [pixel_size*b for b in bins]
+    # Start analysis
+    d_r = annulus_width = 0.5
+    # Generate list of annular radii
+    detector_axis_length = data2d.shape[0] * pixel_size
+    n_bins = int(detector_axis_length / annulus_width)
+    radii = numpy.linspace(0, detector_axis_length, n_bins)
 
-    # Convert bins in units of length to scatting angle
-    angles = [math.atan(r/sample_to_detector) for r in radii]
+    reduced_data = {'Q': numpy.empty(1),
+                    'scattered_intensity': numpy.empty(1),
+                    'scattered_intensity_std': numpy.empty(1),
+                    '<Q>': numpy.empty(1),
+                    'Q_variance': numpy.empty(1),
+                    'BS': numpy.empty(1, dtype=int)}
+    ordinate = numpy.empty(1)
+    for r_0 in radii:
+        if r_0 == 0.0:
+            continue
+        q, v_q = get_q_statistics(r_0, d_r, b_s, wl, wl_spread, sigma_d, l_1, l_2, s_1, s_2)
+        reduced_data['Q'] = numpy.append(reduced_data['Q'], q)
+        reduced_data['Q_variance'] = numpy.append(reduced_data['Q_variance'], v_q)
 
-    # Convert bins in units of scattering angle to Q value
-    qs = [4 * np.pi / wavelength * math.sin(theta / 2) for theta in angles]
+        intensity, intensity_std = get_scattered_intensity(data2d, center, r_0, d_r)
+        reduced_data['scattered_intensity'] = numpy.append(reduced_data['scattered_intensity'], intensity)
+        reduced_data['scattered_intensity_std'] = numpy.append(reduced_data['scattered_intensity_std'], intensity_std)
 
-    # Create a figure of abs. Intensity vs Q
-    fig, ax = plt.subplots()
-    ax.bar(qs[:-1], intensities, align='edge', width=qs[1]-qs[0])
-    ax.set_xlabel("Q (1/angstrom)")
-    ax.set_ylabel("Scattered Intensity (1/cm)")
-    ax.set_xscale('log')
-    # ax.set_yscale('log')
+        bs_factor = get_beam_stop_factor(r_0, d_r, b_s)
+        reduced_data['BS'] = numpy.append(reduced_data['BS'], bs_factor)
 
-    plt.show()
+        ordinate = numpy.append(ordinate, r_0)