806e4d954bae2f648ae60ca8691e5ff5a035c930
burkeds <burkeds@mcmaster.ca>
2023-02-24 09:48:38 -0500

    This branch is for the development of interactive, self-updating figures in Jupyter.

---
+++ cnbl/visualization/jupyter/figures.py
@@ -0,0 +1,11 @@
+"""
+Methods to create interactive figures within Jupyter notebooks.
+
+    author: Devin Burke
+
+(c) Copyright 2023, McMaster University
+"""
+
+var = 10
+class jupyter_figure():
+    pass
