466e91261a7c80e500af8e4c80c45396577a980c
Devin Burke <burkeds@mcmaster.ca>
2023-03-27 13:07:50 -0400

    Changes to print_impact_matrix to allow better interactivity in jupyter.

--- ccns/utils.py
+++ ccns/utils.py
@@ -13,21 +13,15 @@ import numpy as np
 def print_impact_matrix(dat, title=None):
     import matplotlib.pyplot as plt
     h = dat
-    fig = plt.figure(figsize=(6, 3.2))
+    fig = plt.figure()
 
-    ax = fig.add_subplot(111)
+    ax = fig.add_subplot()
     if title:
         ax.set_title(title)
     else:
         ax.set_title('Impact Matrix')
     plt.imshow(h)
     ax.set_aspect('equal')
-
-    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
-    cax.get_xaxis().set_visible(False)
-    cax.get_yaxis().set_visible(False)
-    cax.patch.set_alpha(0)
-    cax.set_frame_on(False)
     plt.colorbar(orientation='vertical')
     plt.show()
 
