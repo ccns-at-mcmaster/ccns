b6d29616b77f37eb625cb16ab5538450c941eb14
Devin Burke <burkeds@mcmaster.ca>
2023-03-30 09:47:54 -0400

    print_impact_matrix will now take all of the arguments accepted by matplotlib.axes.Axes.imshow

--- ccns/utils.py
+++ ccns/utils.py
@@ -6,31 +6,36 @@ laboratory at McMaster University
 
 (c) Copyright 2022, McMaster University
 """
-
 from scipy.integrate import cumtrapz
 import numpy as np
 
-def print_impact_matrix(dat, title=None):
+
+def print_impact_matrix(dat, title='Impact Matrix', cmap=None, norm=None, aspect=None, interpolation=None, alpha=None,
+                        vmin=None, vmax=None, origin=None, extent=None, interpolation_stage=None, filternorm=True,
+                        filterrad=4.0, resample=None, url=None, data=None, **kwargs):
     import matplotlib.pyplot as plt
     h = dat
     fig = plt.figure()
 
     ax = fig.add_subplot()
-    if title:
-        ax.set_title(title)
-    else:
-        ax.set_title('Impact Matrix')
-    plt.imshow(h)
+    ax.set_title(title)
+
+    plt.imshow(h, cmap=cmap, norm=norm, aspect=aspect, interpolation=interpolation, alpha=alpha, vmin=vmin, vmax=vmax,
+               origin=origin, extent=extent, interpolation_stage=interpolation_stage, filternorm=filternorm,
+               filterrad=filterrad, resample=resample, url=url, data=data, **kwargs)
+
     ax.set_aspect('equal')
     plt.colorbar(orientation='vertical')
     plt.show()
+
 
 def normalize(array):
     integral = cumtrapz(array).sum()
     normalized_array = [float(i)/float(integral) for i in array]
     return normalized_array
 
+
 def get_projections(a):
     y = np.mean(a, axis=0)
     x = np.mean(a, axis=1)
-    return y,x
+    return y, x
