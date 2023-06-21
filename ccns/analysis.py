f09d1f59e7ac0ba3dbc9bf95f6005b7c9199472d
burkeds <burkeds@mcmaster.ca>
2023-06-21 10:44:31 -0400

    Fixed placement of method.

--- ccns/analysis.py
+++ ccns/analysis.py
@@ -45,7 +45,7 @@ def _get_guinier_plot(x, q_range=None):
     y_label = r'${\rm Ln(I)}$'
 
     if isinstance(q_range, slice):
-        xr = numpy.log(xr.sel(name='I', Q=q_range), method='nearest')
+        xr = numpy.log(xr.sel(name='I', Q=q_range, method='nearest'))
     if q_range is None:
         xr = numpy.log(xr.sel(name='I'))
 
@@ -80,7 +80,7 @@ def _get_porod_plot(x, q_range=None):
     y_label = 'Log(I(Q)-B)'
 
     if isinstance(q_range, slice):
-        xr = numpy.log10(xr.sel(name='I', Q=q_range), method='nearest')
+        xr = numpy.log10(xr.sel(name='I', Q=q_range, method='nearest'))
     if q_range is None:
         xr = numpy.log10(xr.sel(name='I'))
 
